c00a80ef2da52e8973be5aee53dd581fa2f39ea1
Glitch (timestamp30) <none>
2018-04-01 15:32:42 +0000

    🎃🚊 Checkpoint ./views/index.html:614787/1920 ./server.js:614787/2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.set("/", (request, response) => {
+app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
